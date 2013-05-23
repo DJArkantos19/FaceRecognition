7ff0c72476cc6534d84c916edf2d6f24c19d54ca
Pablo Rodriguez <pablins6101@hotmail.com>
2013-05-23 10:51:40 -0500

    Se crearon dos cuadrados para el fondo de pantalla en el main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+--Creamos un cuadro (default blanco) 
+local cuadro1 = display.newRect(0, 0, display.contentWidth, 200)
+ -- Creamos otro cuadro
+local cuadro2 = display.newRect(0, 200, display.contentWidth, display.contentHeight - 200, y2)
+--cambiamos el color a verde
+cuadro2:setFillColor(0, 129, 0)
