bfbafd60828bc96b66d28eba89fd89465738627f
Pablo Rodriguez <pablins6101@hotmail.com>
2013-05-23 11:04:59 -0500

    Se crearon dos cuadrados para el fondo de pantalla en el main.lua y se corriguieron los errores presentados con el wilcho Estoy pendejeando porque no puedo subir mis cambios

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 display.setStatusBar( display.HiddenStatusBar )
 
 --Creamos un cuadro (default blanco) 
-local cuadro1 = display.newRect(0, 0, display.contentWidth, 200)
+local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
  -- Creamos otro cuadro
-local cuadro2 = display.newRect(0, 200, display.contentWidth, display.contentHeight - 200, y2)
+local cuadro2 = display.newRect(0, 200, display.contentWidth, display.contentHeight - 200, display.contentHeight-200)
 --cambiamos el color a verde
 cuadro2:setFillColor(0, 129, 0)
