e3de2501df930396b7e3adac554090f1c8491b26
Wilmer Garcia <garciaswa15@hotmail.com>
2013-05-23 12:09:36 -0500

    agreagado texto...

--- main.lua
+++ main.lua
@@ -12,15 +12,17 @@ local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
 --cambiamos el color a verde
 
 local texto = display.newText("SMART Apps... BE SMART!!!", 80, 50,TimesNewRoman,39)
-texto:setTextColor(0,0,250)
+texto:setTextColor(15,50,250)
 
 local texto = display.newText("INGENIEROS", 30, 350,TimesNewRoman,45)
+texto:setTextColor(15,50,250)
 local texto = display.newText("Pablo Rodriguez", 80, 450,TimesNewRoman,24)
 
-
+texto:setTextColor(15,50,250)
 local texto = display.newText("El wilcho", 80, 200,TimesNewRoman,24)
-
+texto:setTextColor(15,50,250)
 local texto = display.newText("David Torres", 80, 420,TimesNewRoman,24)
+texto:setTextColor(15,50,250)
 local texto = display.newText("Wilmer Garcia", 80, 390,TimesNewRoman,24)
 
-
+texto:setTextColor(15,50,250)
