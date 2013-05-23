6a15424e8e1ebd30f18117e73647bce01f03b503
Wilmer Garcia <garciaswa15@hotmail.com>
2013-05-23 12:07:35 -0500

    agreagado texto...

--- main.lua
+++ main.lua
@@ -11,8 +11,6 @@ local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
  -- Creamos otro cuadro
 --cambiamos el color a verde
 
-cuadro2:setFillColor(0, 129, 0)
-
 local texto = display.newText("SMART Apps... BE SMART!!!", 80, 50,TimesNewRoman,39)
 texto:setTextColor(0,0,250)
 
