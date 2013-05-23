97c1e5d09361139a0de5a8b3ba0831136034c46f
Wilmer Garcia <garciaswa15@hotmail.com>
2013-05-23 11:53:51 -0500

    agregado scena inicio y story board

---
+++ inicio.lua
@@ -0,0 +1,14 @@
+
+
+local storyboard = require ("storyboard")
+local scene = storyboard.newScene()
+
+	function scene:createScene(event)
+
+	local screenGroup = self.view
+
+	background = display.newImage("start.png")
+	screenGroup:insert(background)
+
+    
+end
--- main.lua
+++ main.lua
@@ -2,11 +2,16 @@
 
 display.setStatusBar( display.HiddenStatusBar )
 
+local storyboard = require "storyboard"
+storyboard.gotoScene( "start" )
+
 --Creamos un cuadro (default blanco) 
 local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
+
  -- Creamos otro cuadro
 local cuadro2 = display.newRect(0, 200, display.contentWidth, display.contentHeight - 200, display.contentHeight-200)
 --cambiamos el color a verde
+
 cuadro2:setFillColor(0, 129, 0)
 
 local texto = display.newText("SMART Apps... BE SMART!!!", 80, 50,TimesNewRoman,39)
