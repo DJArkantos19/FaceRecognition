0f586cec8edf0d8a29958a653e4f8697dfaf32d8
Davids <Davids@Davids-PC>
2013-05-23 11:34:57 -0500

    Fondo!!

---
+++ level2.lua
@@ -0,0 +1,3 @@
+local background = display.newImageRect( "background.png", display.contentWidth, display.contentHeight )
+	background:setReferencePoint( display.TopLeftReferencePoint )
+	background.x, background.y = 0, 0
