9bedb15bab9618d32e9acf26ffd16e447c6499a3
Wilmer Garcia <garciaswa15@hotmail.com>
2013-05-23 12:06:00 -0500

    agreagado texto...

--- main.lua
+++ main.lua
@@ -9,7 +9,6 @@ local background = display.newImageRect( "background.png", display.contentWidth,
 local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
 
  -- Creamos otro cuadro
-local cuadro2 = display.newRect(0, 200, display.contentWidth, display.contentHeight - 200, display.contentHeight-200)
 --cambiamos el color a verde
 
 cuadro2:setFillColor(0, 129, 0)
