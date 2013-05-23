9dc47539068d065727c050483f914b3e017325b5
Wilmer Garcia <garciaswa15@hotmail.com>
2013-05-23 11:56:00 -0500

    agregado scena inicio y story board

--- level1.lua
+++ level1.lua
@@ -1,3 +1,5 @@
 --aqui quito la barrita... esto lo hizo el pablins
 display.setStatusBar( display.HiddenStatusBar )
 
+local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
+
--- main.lua
+++ main.lua
@@ -2,8 +2,15 @@
 
 display.setStatusBar( display.HiddenStatusBar )
 
+<<<<<<< HEAD
 local storyboard = require "storyboard"
 storyboard.gotoScene( "start" )
+=======
+
+local background = display.newImageRect( "background.png", display.contentWidth, display.contentHeight )
+	background:setReferencePoint( display.TopLeftReferencePoint )
+	background.x, background.y = 0, 0
+>>>>>>> branch 'master' of http://github.com/DJArkantos19/FaceRecognition.git
 
 --Creamos un cuadro (default blanco) 
 local cuadro1 = display.newRect(0, 0, display.contentWidth, 200 )
@@ -18,4 +25,9 @@ local texto = display.newText("SMART Apps... BE SMART!!!", 80, 50,TimesNewRoman,
 texto:setTextColor(0,0,250)
 
 local texto = display.newText("Pablo Rodriguez", 80, 450,TimesNewRoman,24)
+<<<<<<< HEAD
 local texto = display.newText("El wilcho", 80, 200,TimesNewRoman,24)
+=======
+
+
+>>>>>>> branch 'master' of http://github.com/DJArkantos19/FaceRecognition.git
