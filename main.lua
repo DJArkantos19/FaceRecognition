5f32f8fe4ccf3141c4f1af9a7754891f659d1ad8
Pablo Rodriguez <pablins6101@hotmail.com>
2013-05-23 11:49:29 -0500

    En el main agregue el nombre de los ingenieros desarrolladores

--- main.lua
+++ main.lua
@@ -12,4 +12,9 @@ cuadro2:setFillColor(0, 129, 0)
 local texto = display.newText("SMART Apps... BE SMART!!!", 80, 50,TimesNewRoman,39)
 texto:setTextColor(0,0,250)
 
+local texto = display.newText("INGENIEROS", 30, 350,TimesNewRoman,45)
 local texto = display.newText("Pablo Rodriguez", 80, 450,TimesNewRoman,24)
+local texto = display.newText("David Torres", 80, 420,TimesNewRoman,24)
+local texto = display.newText("Wilmer Garcia", 80, 390,TimesNewRoman,24)
+
+
